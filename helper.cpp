434b6540c341ef53f07e5617b7dcea675bd8242a
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-04-22 22:59:29 -0700

    file commit

---
+++ helper.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <stdio.h>
+
+using namespace std;
+
+void list_choices() {
+  std::cout << "1. List Food" << std::endl <<
+  "2. Add item" << std::endl <<
+  "3. Delete item" << std::endl <<
+  "4. Place Order" << std::endl <<
+  "5. Exit" << std::endl <<
+  std::endl << "menu>> ";
+}
+
+char *convert(const std::string & s)
+{
+   char *pc = new char[s.size()+1];
+   std::strcpy(pc, s.c_str());
+   return pc;
+}
+
+string split_up_item(string s) {
+  istringstream iss(s, istringstream::in);
+  string buf;
+  std::vector<std::string> item;
+  // converting vector to char array
+  std::vector<char*>  r_item;
+  string food_item;
+
+  while(iss >> buf) {
+      iss >> buf;
+      // put split strings into an array
+      item.push_back(buf);
+  }
+
+  // storing first index to string
+  std::transform(item.begin(), item.end(), std::back_inserter(r_item), convert);
+  for ( int i = 0 ; i < r_item.size() ; i++ ) {
+    food_item = r_item[0];
+  }
+  return food_item;
+}
+
+char *split_up_money(string s) {
+  istringstream iss(s, istringstream::in);
+  string buf;
+  std::vector<std::string> item;
+  // converting vector to char array
+  std::vector<char*>  m_item;
+  char *money_item;
+
+  while(iss >> buf) {
+      iss >> buf;
+      item.push_back(buf);
+  }
+
+  // storing third index to string to be converted to integer
+  std::transform(item.begin(), item.end(), std::back_inserter(m_item), convert);
+  for ( int i = 0 ; i < m_item.size() ; i++ ) {
+    money_item = m_item[2];
+  }
+  return money_item;
+}
+
+void receipt(float total_price, string food_array) {
+  FILE *fp;
+  fp = fopen("receipt.txt", "w+");
+  fprintf(fp, "Total Price $%f\n\n", total_price);
+  fprintf(fp, "Food Ordered: %s\n", food_array.c_str());
+  fclose(fp);
+}
+
+float couponed_code(float total_price) {
+  ifstream cc("/tmp/coupon");
+  char buffer[1024];
+  char new_buf[1024];
+
+  if(cc.is_open()) {
+    std::cout << "Secret Coupon Code Area!" << std::endl;
+
+    while(cc >> buffer) {
+      std::memcpy(new_buf, buffer, sizeof(buffer));
+    }
+
+    if(!memcmp(new_buf, "\xde\xad\xbe\xef", 4)) {
+      std::cout << "You got 15 percent off your order." << std::endl;
+      total_price = total_price - (total_price * 0.15);
+      std::cout << "Your new total: " << total_price << std::endl << std::endl;
+
+      // Delete coupon
+      remove("/tmp/coupon");
+    } else {
+      std::cout << "Better luck next time." << std::endl << std::endl;
+    }
+  } else {
+    std::cout << "Can't file for coupon in /tmp directory!" << std::endl << std::endl;
+  }
+  return total_price;
+}
---
+++ main.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <iomanip>
+#include "helper.hpp"
+
+using namespace std;
+
+typedef struct {
+  string food_1;
+  string food_2;
+  string food_3;
+  string food_4;
+} split_food_items;
+
+int main() {
+  string menu_choice, food_choice, remove_choice;
+  float total = 0.00;
+  string ordered;
+  char food[4][100] = {"1. Hamburger : $ 3.25", "2. Grilled-Cheese : $ 2.50",
+              "3. Fries : $ 2.00", "4. Soda : $ 1.00"};
+
+  split_food_items f;
+  f.food_1 = split_up_item(food[0]);
+  f.food_2 = split_up_item(food[1]);
+  f.food_3 = split_up_item(food[2]);
+  f.food_4 = split_up_item(food[3]);
+
+  while(1) {
+    std::cout << "Total Price: $" << std::fixed <<
+    std::setprecision(2) << total << endl;
+    std::cout << "Ordered: " << ordered << endl << endl;
+
+    list_choices();
+    getline(cin, menu_choice);
+
+    if(menu_choice == "1") {
+      cout << "Food & Drink Choices:" << std::endl;
+
+      for(int i = 0; i < 4; i++) {
+        std::cout << food[i] << std::endl;
+      }
+      cout << std::endl;
+
+    } else if(menu_choice == "2") {
+      cout << std::endl << "food>> ";
+      getline(cin, food_choice);
+
+      if(food_choice == "1") {
+        std::cout << "Adding " << f.food_1 << " to your order." << endl;
+        total += atof(split_up_money(food[0]));
+        ordered += f.food_1 + ", ";
+      } else if(food_choice == "2") {
+        std::cout << "Adding " << f.food_2 << " to your order." << endl;
+        total += atof(split_up_money(food[1]));
+        ordered += f.food_2 + ", ";
+      } else if(food_choice == "3") {
+        std::cout << "Adding " << f.food_3 << " to your order." << endl;
+        total += atof(split_up_money(food[2]));
+        ordered += f.food_3 + ", ";
+      } else if(food_choice == "4") {
+        std::cout << "Adding " << f.food_4 << " to your order." << endl;
+        total += atof(split_up_money(food[3]));
+        ordered += f.food_4 + ", ";
+      } else {
+        std::cout << "Invalid Choice." << std::endl;
+      }
+    } else if(menu_choice == "3") {
+      if(total > 0.01) {
+        // 0000000100006f0a is where to break here
+        cout << std::endl << "remove>> ";
+        getline(cin, remove_choice);
+
+        // 0000000100007148         cmp        esi, 0x0
+        if(remove_choice == "1" && ordered.find(f.food_1) != string::npos) {
+          std::cout << "Removing " << f.food_1 << " from your order." << endl;
+          total -= atof(split_up_money(food[0]));
+
+          size_t pos = ordered.find(f.food_1);
+          ordered.erase(pos, f.food_1.length());
+        } else if(remove_choice == "2" && ordered.find(f.food_2) != string::npos) {
+          std::cout << "Removing " << f.food_2 << " from your order." << endl;
+          total -= atof(split_up_money(food[1]));
+
+          size_t pos = ordered.find(f.food_2);
+          ordered.erase(pos, f.food_2.length());
+        } else if(remove_choice == "3" && ordered.find(f.food_3) != string::npos) {
+          std::cout << "Removing " << f.food_3 << " from your order." << endl;
+          total -= atof(split_up_money(food[2]));
+
+          size_t pos = ordered.find(f.food_3);
+          ordered.erase(pos, f.food_3.length());
+        } else if(remove_choice == "4" && ordered.find(f.food_4) != string::npos) {
+          std::cout << "Removing " << f.food_4 << " from your order." << endl;
+          total -= atof(split_up_money(food[3]));
+
+          size_t pos = ordered.find(f.food_4);
+          ordered.erase(pos, f.food_4.length());
+        } else {
+          std::cout << "Invalid Choice." << std::endl;
+        }
+      } else {
+        std::cout << "You haven't ordered anything." << std::endl;
+      }
+    } else if(menu_choice == "4") {
+      std::cout << "Storing receipt to text file.." << std::endl;
+      receipt(total, ordered);
+    } else if(menu_choice == "5") {
+      exit(-1);
+    } else if(menu_choice == "6") {
+      total = couponed_code(total);
+    } else {
+      std::cout << "Invalid Choice." << std::endl;
+    }
+  }
+  return 0;
+}
