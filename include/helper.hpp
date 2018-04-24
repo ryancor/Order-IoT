598b13c62c74e7aaad976d2c0f0ccd137388afbb
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-04-24 15:06:09 -0700

    organize all headers

---
+++ include/helper.hpp
@@ -0,0 +1,9 @@
+#include <string>
+
+void list_choices();
+char *convert(const std::string);
+std::string split_up_item(std::string);
+char *split_up_money(std::string);
+void receipt(float, std::string);
+float couponed_code(float);
+void clear_cin();
---
+++ include/requests.hpp
@@ -0,0 +1,5 @@
+#include <string>
+
+bool is_host_up(const std::string&, int);
+void post_data(const time_t, float, std::string);
+void boost_post_data(const time_t, float, std::string);
