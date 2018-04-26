267388a5bf90b70afd50d6d8f1071c49788d7dcc
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-04-26 09:51:05 -0700

    add unsigned long value

--- include/helper.hpp
+++ include/helper.hpp
@@ -4,6 +4,7 @@ void list_choices();
 char *convert(const std::string);
 std::string split_up_item(std::string);
 char *split_up_money(std::string);
-void receipt(float, std::string);
+void receipt(unsigned long, float, std::string);
 float couponed_code(float);
 void clear_cin();
+unsigned long customer_id();
