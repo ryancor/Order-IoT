f6a94c1af01fce2c9812cfc76302457f575b7dac
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-04-27 11:26:42 -0700

    char pointer to query name

--- include/helper.hpp
+++ include/helper.hpp
@@ -4,7 +4,7 @@ void list_choices();
 char *convert(const std::string);
 std::string split_up_item(std::string);
 char *split_up_money(std::string);
-void receipt(unsigned long, float, std::string);
+void receipt(unsigned long, char*, float, std::string);
 float couponed_code(float);
 void clear_cin();
 unsigned long customer_id();
--- include/requests.hpp
+++ include/requests.hpp
@@ -1,5 +1,6 @@
 #include <string>
 
 bool is_host_up(const std::string&, int);
-void post_data(const time_t, unsigned long, float, std::string);
+void post_data(const time_t, unsigned long,  char *, float, std::string);
 void boost_post_data(const time_t, float, std::string);
+char *user_request();
