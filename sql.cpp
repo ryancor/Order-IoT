7024f938c65c74f8179433b989e8b46139da089f
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-05-09 15:20:49 -0700

    retrval of tbl data

---
+++ sql.cpp
@@ -0,0 +1,90 @@
+#include <cstdlib>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <mysql/mysql.h>
+#include <mysql/my_global.h>
+#include <string>
+
+using namespace std;
+
+void finish_with_error(MYSQL *con) {
+  fprintf(stderr, "%s\n", mysql_error(con));
+  mysql_close(con);
+  exit(1);
+}
+
+void start_tbl(MYSQL *con, const char * HOST, const char * USER, const char * PASSWORD, const char * DATABASE) {
+  if (mysql_real_connect(con, HOST, USER, PASSWORD, DATABASE, 0, NULL, 0) == NULL)  {
+      std::cout << "Please start your MySQL DB." << std::endl <<
+      "View README.md for instructions." << std::endl;
+  }
+
+  if (mysql_query(con, "DROP TABLE IF EXISTS Orders")) {
+      finish_with_error(con);
+  }
+
+  if (mysql_query(con, "CREATE TABLE Orders(Id INT, Name TEXT)")) {
+      finish_with_error(con);
+  }
+
+  if (mysql_query(con, "INSERT INTO Orders VALUES(1,'1. Hamburger : $ 3.25')")) {
+      finish_with_error(con);
+  }
+
+  if (mysql_query(con, "INSERT INTO Orders VALUES(2,'2. Grilled-Cheese : $ 2.50')")) {
+      finish_with_error(con);
+  }
+
+  if (mysql_query(con, "INSERT INTO Orders VALUES(2,'3. Fries : $ 2.00')")) {
+      finish_with_error(con);
+  }
+
+  if (mysql_query(con, "INSERT INTO Orders VALUES(3,'4. Soda : $ 1.00')")) {
+      finish_with_error(con);
+  }
+}
+
+const string DataGet() {
+  MYSQL *con = mysql_init(NULL);
+  string new_str;
+
+  if (con == NULL) {
+      fprintf(stderr, "mysql_init() failed\n");
+      exit(1);
+  }
+
+  start_tbl(con, "localhost", "ryancor", "qwerty", "ordersdb");
+
+  if (mysql_query(con, "SELECT Name FROM Orders")) {
+      finish_with_error(con);
+  }
+
+  MYSQL_RES *result = mysql_store_result(con);
+
+  if (result == NULL) {
+      finish_with_error(con);
+  }
+
+  int num_fields = mysql_num_fields(result);
+
+  MYSQL_ROW row;
+
+  while((row = mysql_fetch_row(result))) {
+      for(int i = 0; i < num_fields; i++) {
+          new_str += row[i] ? row[i] : "NULL";
+          new_str += " ";
+      }
+      new_str += " ";
+  }
+
+  mysql_free_result(result);
+  mysql_close(con);
+
+  return new_str;
+}
+
+void send_data(void) {
+  string get = DataGet();
+  std::cout << "Data: " << get << std::endl;
+}
