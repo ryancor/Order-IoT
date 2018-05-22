b331c2a61aa3b4fc437d4727106e3ba227e58d7a
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-05-22 09:19:23 -0700

    namespace patch for to_string()

--- include/requests.hpp
+++ include/requests.hpp
@@ -5,3 +5,12 @@ void post_data(const time_t, unsigned long,  char *, float, std::string);
 void boost_post_data(const time_t, float, std::string);
 char *user_request();
 bool open_or_closed();
+
+// This patch is for linux because to_string does not exist there
+namespace patch {
+  template <typename T> std::string to_string(const T& n) {
+    std::ostringstream stm;
+    stm << n;
+    return stm.str();
+  }
+}
