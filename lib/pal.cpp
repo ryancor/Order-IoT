b1fc326cb2ca6a49dcf3fede8123cc190e24494d
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-05-22 17:12:26 -0700

    convert asm function to char

--- lib/pal.cpp
+++ lib/pal.cpp
@@ -1,6 +1,8 @@
 #include "pal.hpp"
 #include <string.h>
 
+extern "C" char read();
+
 bool isPalindrome(char* word) {
 	bool ret = true;
 
@@ -9,9 +11,14 @@ bool isPalindrome(char* word) {
 	char *q = &word[len-1];
 
 	for(int i = 0; i < len; ++i, ++p, --q) {
-		if(*p != *q) {
-			ret = false;
-		}
+	  if(*p != *q) {
+    	ret = false;
+	  }
 	}
 	return ret;
+}
+
+unsigned char readKey() {
+	char(*read_ptr)() = &read;
+	return (*read_ptr)();
 }
