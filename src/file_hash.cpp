11699781b0555fe96183045b94d3503262eb2587
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-05 09:29:29 -0700

    modulize the hash print

--- src/file_hash.cpp
+++ src/file_hash.cpp
@@ -10,18 +10,22 @@
 #include <string>
 
 #include <openssl/md5.h>
+#include <openssl/sha.h>
 
 #include "../include/requests.hpp"
 
 using namespace std;
 
-unsigned char result[MD5_DIGEST_LENGTH];
+unsigned char md5_result[MD5_DIGEST_LENGTH];
+unsigned char sha_result[SHA256_DIGEST_LENGTH];
 
-string md5_print(unsigned char *md) {
+string hash_print(unsigned char *md, size_t length) {
   int i;
   string res;
 
-  for(i = 0; i < MD5_DIGEST_LENGTH; i++) {
+  printf("[+] ");
+
+  for(i = 0; i < length; i++) {
     printf("%02x", md[i]);
     // short converts 0xA to 41 as string
     res.append(patch::to_string((short)md[i]));
@@ -39,7 +43,7 @@ unsigned long get_size_by_fd(int fd) {
   return statbuf.st_size;
 }
 
-string return_hash(char *file) {
+string return_md5_hash(char *file) {
   int file_descript;
   unsigned long file_size;
   void* file_buffer;
@@ -53,8 +57,31 @@ string return_hash(char *file) {
   std::cout << "File size: " << file_size << "kb" << std::endl;
 
   file_buffer = mmap(0, file_size, PROT_READ, MAP_SHARED, file_descript, 0);
-  MD5((unsigned char*)file_buffer, file_size, result);
+  MD5((unsigned char*)file_buffer, file_size, md5_result);
   munmap(file_buffer, file_size);
 
-  return md5_print(result);
+  printf("------------------------\n");
+  printf("MD5 & SHA256 Hash Below\n");
+  printf("------------------------\n");
+
+  return hash_print(md5_result, MD5_DIGEST_LENGTH);
+}
+
+string return_sha_hash(char *file) {
+  int file_descript;
+  unsigned long file_size;
+  void* file_buffer;
+
+  file_descript = open(file, O_RDONLY);
+  if(file_descript < 0) {
+    std::cout << "Can't open file for hash." << std::endl;
+  }
+
+  file_size = get_size_by_fd(file_descript);
+
+  file_buffer = mmap(0, file_size, PROT_READ, MAP_SHARED, file_descript, 0);
+  SHA256((unsigned char*)file_buffer, file_size, sha_result);
+  munmap(file_buffer, file_size);
+
+  return hash_print(sha_result, SHA256_DIGEST_LENGTH);
 }
