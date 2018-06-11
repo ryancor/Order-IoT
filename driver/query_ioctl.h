49f9eaab5d87604b258355614b3b422d0e8bde68
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-11 16:25:08 -0700

    change sequence number of IOW to 1

--- driver/query_ioctl.h
+++ driver/query_ioctl.h
@@ -6,6 +6,8 @@ typedef struct {
   int price;
 } query_arg_t;
 
-#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
+// _IOW(int type, int number, data_type) - similar to _IOR,
+// but used to write data to the driver.
+#define QUERY_SET_VARIABLES _IOW('q', 1, query_arg_t *)
 
 #endif
