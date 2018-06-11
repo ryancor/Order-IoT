0cad465835bc3fb80e4e93b2aba6812e95e761bb
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-11 11:40:16 -0700

    install script to include drivers

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  cd driver
+  make
+  sudo insmod query_ioctl.ko
+  # change ryancor to your local username
+  sudo chown ryancor:ryancor /dev/query_driver
+  cd ../
+fi
 cd lib
 make
 make clean
