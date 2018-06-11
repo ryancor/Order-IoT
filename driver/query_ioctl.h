330decb977efd44c98d3c32ed7d758ab7511ccfc
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-11 16:30:55 -0700

    IOW sequence number

--- driver/query_ioctl.h
+++ driver/query_ioctl.h
@@ -8,6 +8,6 @@ typedef struct {
 
 // _IOW(int type, int number, data_type) - similar to _IOR,
 // but used to write data to the driver.
-#define QUERY_SET_VARIABLES _IOW('q', 1, query_arg_t *)
+#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
 
 #endif
