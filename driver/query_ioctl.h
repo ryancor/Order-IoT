815ad6233330650e83eeb5019509036f10dd90c9
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-13 11:01:53 -0700

    adding IOR

--- driver/query_ioctl.h
+++ driver/query_ioctl.h
@@ -3,12 +3,14 @@
 #include <linux/ioctl.h>
 
 typedef struct {
+  int size_of_all;
   char *order;
   int price;
 } query_arg_t;
 
-// _IOW(int type, int number, data_type) - similar to _IOR,
+// _IOW(int type, int number, data_type) - similar to _IOR (read),
 // but used to write data to the driver.
+#define QUERY_GET_VARIABLES _IOR('q', 1, query_arg_t *)
 #define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
 
 #endif
