b25bbf03f6c3761c8df7492ca79d83a13cd2a2ee
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-18 11:16:01 -0700

    read from int* mmap

---
+++ src/linux/kern.cpp
@@ -0,0 +1,32 @@
+#if __unix__
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <errno.h>
+  #include <fcntl.h>
+  #include <unistd.h>
+  #include <sys/mman.h>
+
+  #define PAGE_SIZE     4096
+  #define KERNEL_FILE   "/sys/kernel/debug/query_vma"
+
+  void read_sys() {
+    if(getuid() == 0) {
+      int configfd;
+      int *address;
+
+      configfd = open(KERNEL_FILE, O_RDWR);
+      if(configfd < 0) {
+        perror("open");
+      }
+
+      address = (int*)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, configfd, 0);
+      if(address == MAP_FAILED) {
+        perror("mmap");
+      }
+
+      printf("\n\nSpecial root initial message: %p\n\n\n", address);
+
+      close(configfd);
+    }
+  }
+#endif
