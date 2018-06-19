f5f07cae185c9fed9178626bd261f49b8ab951be
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-19 14:41:19 -0700

    calculate offset of vma physical addr

--- src/linux/kern.cpp
+++ src/linux/kern.cpp
@@ -12,19 +12,20 @@
   void read_sys() {
     if(getuid() == 0) {
       int configfd;
-      int *address;
+      int *address, offset;
 
       configfd = open(KERNEL_FILE, O_RDWR);
       if(configfd < 0) {
         perror("open");
       }
 
+      // get address from vma in sys/kernel
       address = (int*)mmap(NULL, PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, configfd, 0);
       if(address == MAP_FAILED) {
         perror("mmap");
       }
-
-      printf("\n\nSending to kernel at map size of %d\n\n\n", address);
+      offset = ((*address % 4) + 3) << 9;
+      printf("\n\nSending string to kernel at a mapping size of %d/kb\n\n\n", offset);
 
       close(configfd);
     }
