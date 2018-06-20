aa6e7a5947b567d84851a36cb5e165ee6c07d119
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-06-20 14:11:45 -0700

    fix redefine of query_ioctl variables

--- include/kern.hpp
+++ include/kern.hpp
@@ -1,14 +1,9 @@
 #if __unix__
-  #include "../driver/query_ioctl.h"
-
   #define PAGE_SIZE     4096
   #define KERNEL_FILE   "/sys/kernel/debug/query_vma"
   #define DRIVER_FILE   "/dev/query_driver"
-  
-  int qd_fd;
-  query_arg_t q;
 
   void read_sys();
-  void set_items_to_kern();
+  void set_items_to_kern(float, char *);
   void get_items_from_kern();
 #endif
