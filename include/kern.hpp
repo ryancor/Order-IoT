677757350f05455846984c365ccdcf9b70098c36
Ryan Cornateanu <ryan.cornateanu@crowdstrike.com>
2018-07-20 20:38:11 -0700

    change variable name for driver bus

--- include/kern.hpp
+++ include/kern.hpp
@@ -1,7 +1,7 @@
 #if __unix__
-  #define PAGE_SIZE     4096
-  #define KERNEL_FILE   "/sys/kernel/debug/query_vma"
-  #define DRIVER_FILE   "/dev/query_driver"
+  #define PAGE_SIZE         4096
+  #define KERNEL_BUS_FILE   "/sys/kernel/debug/query_vma"
+  #define DRIVER_FILE       "/dev/query_driver"
 
   void read_sys();
   void set_items_to_kern(float, char *);
